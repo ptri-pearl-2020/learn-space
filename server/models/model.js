901c0116a7e2d96014c840816b3a035e90705533
alex <allx02@hotmail.com>
2020-06-10 22:36:07 -0400

    connected to db, added tables to db

---
+++ server/models/model.js
@@ -0,0 +1,9 @@
+const { Pool } = require ("pg"); //import pool class from pg package
+require('dotenv').config() //access vars from .env file
+const dbConnect = process.env.DBCONNECTURL; //assign new var to db connection string.
+const pool = new Pool({connectionString: dbConnect});
+
+module.exports = {query:(text, params, callback) => { //when importing modelJS, it will log the queries run
+  console.log('executed query: ', text);
+  return pool.query(text, params, callback); //executes query using dbconnect
+}}
