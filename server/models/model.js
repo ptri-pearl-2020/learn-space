fa7036d08832c0713bf1a97322feb2316f9c74f7
Tariq Khan <digitalgnomes@gmail.com>
2020-06-10 20:05:28 -0700

    Add .eslintrc file and linter setup for the project

--- server/models/model.js
+++ server/models/model.js
@@ -1,9 +1,13 @@
-const { Pool } = require ("pg"); //import pool class from pg package
-require('dotenv').config() //access vars from .env file
-const dbConnect = process.env.DBCONNECTURL; //assign new var to db connection string.
-const pool = new Pool({connectionString: dbConnect});
+const { Pool } = require('pg'); // import pool class from pg package
+require('dotenv').config();
 
-module.exports = {query:(text, params, callback) => { //when importing modelJS, it will log the queries run
-  console.log('executed query: ', text);
-  return pool.query(text, params, callback); //executes query using dbconnect
-}}
+ // access vars from .env file
+const dbConnect = process.env.DBCONNECTURL; // assign new var to db connection string.
+const pool = new Pool({ connectionString: dbConnect });
+
+module.exports = {
+  query: (text, params, callback) => { // when importing modelJS, it will log the queries run
+    console.log('executed query: ', text);
+    return pool.query(text, params, callback); // executes query using dbconnect
+  }
+};
