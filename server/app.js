ddbfbd0bfc670af4914059fbb8c524890efeb4ab
Tariq Khan <digitalgnomes@gmail.com>
2020-06-15 17:18:52 -0700

    Separated backend sever routes in separate routes folder and cleaned up server.js which was renamed to app.js

---
+++ server/app.js
@@ -0,0 +1,62 @@
+/* eslint-disable prettier/prettier */
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve favicon for the site
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// route files
+const loginRoute = require('./routes/login');
+const signUpRoute = require('./routes/signup');
+
+// express app to handle JSON and url encoding
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+
+/**
+ * Server Routes
+ */
+
+// Landing page
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to Learn Space</h1>');
+  res.end();
+});
+
+// Login
+app.use(loginRoute);
+
+// User signup information validation, bcrypt password, return JWT
+app.use(signUpRoute);
+
+// global unknown route handler
+app.get('*', (req, res) => {
+  res.sendStatus(404);
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+  const defaultError = {
+    log: 'Express error handler caught and unknown middleware error',
+    status: 400,
+    message: { err: 'An error has occured' }
+  };
+  const newErr = Object.assign(defaultError, err); // reassigning error
+  console.error(newErr.log);
+  res.status(newErr.status).send(newErr.message);
+
+  return next();
+});
+
+// Listen on port 3000
+app.listen(port);
+
+console.log(`Listening on ${port} at: http://localhost:${3000}`);
