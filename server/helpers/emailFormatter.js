b8dcb312f93f76a0b742243531797340562c6b81
Tariq Khan <digitalgnomes@gmail.com>
2020-06-16 12:24:15 -0700

    Update login route to send JWT to the frontend

--- server/helpers/emailFormatter.js
+++ server/helpers/emailFormatter.js
@@ -1,12 +1,19 @@
 // format email by converting to lowercase, and removing periods in email handle
 const emailFormatter = (userEmail) => {
-  // Parse email to remove '.' and make it case insensitive
-  const emailParts = userEmail.toLowerCase().split('@');
-  const emailName = emailParts[0].replace(/\.+/g, '');
-  emailParts[0] = emailName;
-  const formattedEmail = emailParts.join('@');
+  if (userEmail === undefined) return 'error: email received is undefined';
 
-  return formattedEmail;
+  // Parse email to remove '.' and make it case insensitive
+  try {
+    const emailParts = userEmail.toLowerCase().split('@');
+    const emailName = emailParts[0].replace(/\.+/g, '');
+    emailParts[0] = emailName;
+    const formattedEmail = emailParts.join('@');
+
+    return formattedEmail;
+  } catch (error) {
+    console.error(error.message);
+    return error.message;
+  }
 };
 
 module.exports = emailFormatter;
